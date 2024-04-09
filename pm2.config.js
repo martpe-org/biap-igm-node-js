445df3dd781d734fb2c24ff9fb17809747fb8d41
revanth <108116065+revanth-useline@users.noreply.github.com>
2024-04-09 21:06:30 +0530

    modified pm2 script

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
         name   : "martpe-igm-api",
-        script : "./dist/index.js",
+        script : "npm run start:dev",
         instances: 1,
         max_memory_restart: '300M',
         env_production: {
